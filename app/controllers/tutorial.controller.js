353f86e2e60f69d54fbf052609083dc810781833
Dan Chiev <danchiev2@Gmail.com>
2021-04-13 19:34:01 -0400

    started controller

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,25 @@
+const db = require("../models");
+const Tutorial = db.tutorials;
+
+// create and save tutorial
+exports.create = (req, res) => {};
+
+// retreive all tutorials
+exports.findAll = (req, res) => {};
+
+// find single tutorial with id
+
+exports.findOne = (req, res) => {};
+
+// Update by id
+exports.findOne = (req, res) => {};
+
+// delete a tutorial with id
+exports.delete = (req, res) => {};
+
+// delete all tutorials
+
+exports.deleteAll = (req, res) => {};
+
+// find all tutorials that are published
+exports.findAllPublic = (req, res) => {};
