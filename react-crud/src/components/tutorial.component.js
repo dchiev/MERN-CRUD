eb2659546abf59f64c1c954235fc5e8d687ff772
Dan Chiev <danchiev2@Gmail.com>
2021-05-01 13:17:43 -0400

    added js

---
+++ react-crud/src/components/tutorial.component.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+
+export default class Tutorial extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.getTutorial = this.getTutorial.bind(this);
+    this.updatePublished = this.updatePublished.bind(this);
+    this.updateTutorial = this.updateTutorial.bind(this);
+    this.deleteTutorial = this.deleteTutorial.bind(this);
+
+    this.state = {
+      currentTutorial: {
+        id: null,
+        title: "",
+        description: "",
+        published: false,
+      },
+      message: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getTutorial(this.props.match.params.id);
+  }
+
+  onChangeTitle(e) {
+    const title = e.target.value;
+
+    this.setState(function (prevState) {
+      return {
+        currentTutorial: {
+          ...prevState.currentTutorial,
+          title: title,
+        },
+      };
+    });
+  }
+
+  onChangeDescription(e) {
+    const description = e.target.value;
+
+    this.setState((prevState) => ({
+      currentTutorial: {
+        ...prevState.currentTutorial,
+        description: description,
+      },
+    }));
+  }
+
+  getTutorial(id) {
+    TutorialDataService.get(id)
+      .then((response) => {
+        this.setState({
+          currentTutorial: response.data,
+        });
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  updatePublished(status) {
+    var data = {
+      id: this.state.currentTutorial.id,
+      title: this.state.currentTutorial.title,
+      description: this.state.currentTutorial.description,
+      published: status,
+    };
+
+    TutorialDataService.update(this.state.currentTutorial.id, data)
+      .then((response) => {
+        this.setState((prevState) => ({
+          currentTutorial: {
+            ...prevState.currentTutorial,
+            published: status,
+          },
+        }));
+        console.log(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  updateTutorial() {
+    TutorialDataService.update(
+      this.state.currentTutorial.id,
+      this.state.currentTutorial
+    )
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          message: "The tutorial was updated successfully!",
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  deleteTutorial() {
+    TutorialDataService.delete(this.state.currentTutorial.id)
+      .then((response) => {
+        console.log(response.data);
+        this.props.history.push("/tutorials");
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    // ...
+  }
+}
